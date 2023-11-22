ae6413f77925d3aeae20fe5cabcc2c05ef9ca5bd
manojlamichhane <lammichhanemanoj1@gmail.com>
2023-11-22 15:50:12 -0500

    cors policy added

--- app.py
+++ app.py
@@ -3,52 +3,119 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 from pydantic import BaseModel
-
+import base64
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 from typing import Union
 from fastapi import FastAPI, File, UploadFile
+import tempfile
+import hashlib
+from boto3.dynamodb.conditions import Key
+from fastapi.middleware.cors import CORSMiddleware
+
 
 load_dotenv()
 app = FastAPI()
 fileDict = {}
+fileString = {}
 
-class Item(BaseModel):
-    Image: str
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def index():
-    return 'Hello world'
 
-@app.post("/uploadfile/")
+
+s3_client = boto3.client('s3',aws_access_key_id=os.getenv("ACCESS_KEY"),aws_secret_access_key=os.getenv("SECRET_KEY"))
+client = boto3.client('rekognition',aws_access_key_id=os.getenv("ACCESS_KEY"),aws_secret_access_key=os.getenv("SECRET_KEY"),region_name=os.getenv("REGION"))
+# connect to dynamodb and create a table
+dynamo_client = boto3.resource('dynamodb',aws_access_key_id=os.getenv("ACCESS_KEY"),aws_secret_access_key=os.getenv("SECRET_KEY"),region_name=os.getenv("REGION"))
+
+# table_creation_response = dynamo_client.create_table(
+#         TableName='ImageResponse',
+#         KeySchema=[
+#             {
+#                 'AttributeName': 'image_string',
+#                 'KeyType': 'HASH'  # Partition Key
+#             },
+#         ],
+#         AttributeDefinitions=[
+#             {
+#                 'AttributeName': 'image_string',
+#                 'AttributeType': 'S'  # string data type
+#             },
+
+#         ],
+#         ProvisionedThroughput={
+#             'ReadCapacityUnits': 10,
+#             'WriteCapacityUnits': 10
+#         }
+#     )
+
+# print(table_creation_response)
+
+
+
+@app.post("/uploadfile",status_code=201)
 async def create_upload_file(file: UploadFile):
-
     if file :
-        
         print(file.filename)
+
+        # reading the file
+        contents = await file.read()
+
+        # encoding the image to save in the database for caching purpose
+
+        encoded_image = hashlib.sha256(contents).hexdigest()
+
+        # Save the file locally
+        with tempfile.NamedTemporaryFile(delete=False, suffix=file.filename) as temp_file:
+            temp_file.write(file.file.read())
+            local_file_path = temp_file.name
+        
         fileDict['fileName'] = file.filename
 
-        # file.save(secure_filename(file.filename))
+        fileString[file.filename] = encoded_image
+        # print("encoded_image",encoded_image)    
+        s3_client.upload_file(local_file_path, os.getenv("BUCKET"), file.filename)
 
-        s3_client = boto3.client('s3',aws_access_key_id=os.getenv("ACCESS_KEY"),aws_secret_access_key=os.getenv("SECRET_KEY"))
-    
-        s3_client.upload_file(file.filename, os.getenv("BUCKET"), file.filename)
-
-        return {"Status" : "File uploaded successfully"}   
+        return {"message" : "File uploaded successfully"}   
     else:
-        return {"error": "You're missing one of the following: app_secret, key_id"}
+        return {"message": "You're missing one of the following: app_secret, key_id"}
 
-@app.get('/get-label')
+@app.get('/label',status_code=200)
 def show_custom_labels():
 
-    if bool(fileDict) :
+    table = dynamo_client.Table('ImageResponse')
 
-        client=boto3.client('rekognition',aws_access_key_id=os.getenv("ACCESS_KEY"),aws_secret_access_key=os.getenv("SECRET_KEY"),region_name=os.getenv("REGION"))
+    # look for the response saved in dynamodb
+    response = table.query(KeyConditionExpression=Key('image_string').eq(fileString[fileDict['fileName']]))
 
-        response = client.detect_custom_labels(ProjectVersionArn='arn:aws:rekognition:eu-west-1:088594801781:project/NewPlantDetection/version/NewPlantDetection.2023-10-20T16.49.57/1697835000049',
-            Image={"S3Object": {"Bucket": os.getenv("BUCKET"), "Name": fileDict['fileName']}}, MinConfidence=95,
-        )
+    if(len(response['Items'])>0):
+        return {"data":response["Items"],"location":"cache"}
+    else:    
+        if bool(fileDict) :
 
-        return jsonify({"data":response['CustomLabels']})
-    else:
-        return jsonify({'error':'Make sure you upload the image first'})
+            # response = client.detect_custom_labels(ProjectVersionArn='arn:aws:rekognition:eu-west-1:088594801781:project/NewPlantDetection/version/NewPlantDetection.2023-10-20T16.49.57/1697835000049',
+            #     Image={"S3Object": {"Bucket": os.getenv("BUCKET"), "Name": fileDict['fileName']}}, MinConfidence=95,
+            # )
+
+            # store the response to the dynamodb
+            table.put_item(
+                Item={
+                    'image_string': fileString[fileDict['fileName']],
+                    # 'Name': response['CustomLabels'].Name,
+                    # 'Confidence': response['CustomLabels'].Confidence
+                }
+            )
+
+            response = table.query(KeyConditionExpression=Key('image_string').eq(fileString[fileDict['fileName']]))
+        
+            return {"data":response["Items"],"location":"cloud"}
+
+            # return {"data":response['CustomLabels'],"location":"cloud"}
+        else:
+            return {'message':'Make sure you upload the image first'}
