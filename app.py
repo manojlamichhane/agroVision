0197bd6183905c17bba3eb219518e73f625168e8
manojlamichhane <lammichhanemanoj1@gmail.com>
2023-11-22 21:19:37 -0500

    catche with dynamodb implemented

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ import tempfile
 import hashlib
 from boto3.dynamodb.conditions import Key
 from fastapi.middleware.cors import CORSMiddleware
-
+import math
+import io
 
 load_dotenv()
 app = FastAPI()
@@ -27,60 +28,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
 s3_client = boto3.client('s3',aws_access_key_id=os.getenv("ACCESS_KEY"),aws_secret_access_key=os.getenv("SECRET_KEY"))
 client = boto3.client('rekognition',aws_access_key_id=os.getenv("ACCESS_KEY"),aws_secret_access_key=os.getenv("SECRET_KEY"),region_name=os.getenv("REGION"))
+
 # connect to dynamodb and create a table
 dynamo_client = boto3.resource('dynamodb',aws_access_key_id=os.getenv("ACCESS_KEY"),aws_secret_access_key=os.getenv("SECRET_KEY"),region_name=os.getenv("REGION"))
 
-# table_creation_response = dynamo_client.create_table(
-#         TableName='ImageResponse',
-#         KeySchema=[
-#             {
-#                 'AttributeName': 'image_string',
-#                 'KeyType': 'HASH'  # Partition Key
-#             },
-#         ],
-#         AttributeDefinitions=[
-#             {
-#                 'AttributeName': 'image_string',
-#                 'AttributeType': 'S'  # string data type
-#             },
-
-#         ],
-#         ProvisionedThroughput={
-#             'ReadCapacityUnits': 10,
-#             'WriteCapacityUnits': 10
-#         }
-#     )
-
-# print(table_creation_response)
-
-
-
 @app.post("/uploadfile",status_code=201)
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(file: UploadFile = File(...)):
     if file :
-        print(file.filename)
+        fileDict['fileName'] = file.filename
 
         # reading the file
         contents = await file.read()
-
-        # encoding the image to save in the database for caching purpose
-
-        encoded_image = hashlib.sha256(contents).hexdigest()
-
-        # Save the file locally
-        with tempfile.NamedTemporaryFile(delete=False, suffix=file.filename) as temp_file:
-            temp_file.write(file.file.read())
-            local_file_path = temp_file.name
+        file.file.seek(0)
         
-        fileDict['fileName'] = file.filename
-
+        # encoding the image to save in the database for caching purpose
+        encoded_image = hashlib.sha256(contents).hexdigest()
+        
         fileString[file.filename] = encoded_image
-        # print("encoded_image",encoded_image)    
-        s3_client.upload_file(local_file_path, os.getenv("BUCKET"), file.filename)
+        
+        s3_client.upload_fileobj(file.file, os.getenv("BUCKET"), file.filename)
 
         return {"message" : "File uploaded successfully"}   
     else:
@@ -92,30 +60,26 @@ def show_custom_labels():
     table = dynamo_client.Table('ImageResponse')
 
     # look for the response saved in dynamodb
-    response = table.query(KeyConditionExpression=Key('image_string').eq(fileString[fileDict['fileName']]))
+    responseFromDB = table.query(KeyConditionExpression=Key('image_string').eq(fileString[fileDict['fileName']]))
 
-    if(len(response['Items'])>0):
-        return {"data":response["Items"],"location":"cache"}
+    if(len(responseFromDB['Items'])>0):
+        return {"data":responseFromDB["Items"],"location":"cache"}
     else:    
         if bool(fileDict) :
 
-            # response = client.detect_custom_labels(ProjectVersionArn='arn:aws:rekognition:eu-west-1:088594801781:project/NewPlantDetection/version/NewPlantDetection.2023-10-20T16.49.57/1697835000049',
-            #     Image={"S3Object": {"Bucket": os.getenv("BUCKET"), "Name": fileDict['fileName']}}, MinConfidence=95,
-            # )
-
+            responseFromCloud = client.detect_custom_labels(ProjectVersionArn='arn:aws:rekognition:eu-west-1:088594801781:project/NewPlantDetection/version/NewPlantDetection.2023-10-20T16.49.57/1697835000049',
+                Image={"S3Object": {"Bucket": os.getenv("BUCKET"), "Name": fileDict['fileName']}}, MinConfidence=85,
+            )
+            
             # store the response to the dynamodb
             table.put_item(
                 Item={
                     'image_string': fileString[fileDict['fileName']],
-                    # 'Name': response['CustomLabels'].Name,
-                    # 'Confidence': response['CustomLabels'].Confidence
+                    'Name': responseFromCloud['CustomLabels'][0]["Name"],
+                    'Confidence': math.trunc(responseFromCloud['CustomLabels'][0]['Confidence'])
                 }
             )
 
-            response = table.query(KeyConditionExpression=Key('image_string').eq(fileString[fileDict['fileName']]))
-        
-            return {"data":response["Items"],"location":"cloud"}
-
-            # return {"data":response['CustomLabels'],"location":"cloud"}
+            return {"data":responseFromCloud['CustomLabels'],"location":"cloud"}
         else:
             return {'message':'Make sure you upload the image first'}
